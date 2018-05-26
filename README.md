5933bca4067194e71ae9db885adba766a50a5611
n33t1 <han.wa@husky.neu.edu>
2018-05-26 02:48:56 -0400

    updated readme

--- README.md
+++ README.md
@@ -1,88 +1,29 @@
-[![No Maintenance Intended](http://unmaintained.tech/badge.svg)](http://unmaintained.tech/)
+# jeopardy-parser
+Python crawler for jeopardy games on [J! Archive](http://j-archive.com/).
 
-Jeopardy parser
-===============
-
-Quick note: this project does **not** use semantic versioning (`python parser.py --version` outputs the last updated date of the script).
-
-What is this?
--------------
-
-This is a Python script to extract [Jeopardy!] clues from the [J! Archive] website and dump them into a SQLite database for use elsewhere (no particular application is intended). Python 2.7.* and SQLite 3.7.* on *nix have been tested and confirmed to work (requires BeautifulSoup 4 and the lxml parser).
-
-  [Jeopardy!]:http://www.jeopardy.com/
-  [J! Archive]:http://j-archive.com/
-
-Quick start
------------
-
+## Setup
 ```bash
-git clone git://github.com/whymarrh/jeopardy-parser.git
+git clone https://github.com/n33t1/jeopardy-parser.git
 cd jeopardy-parser
 pip install -r requirements.txt
-python download.py
-python parser.py
 ```
 
-How long will all this take?
-----------------------------
+This crawler provides 2 kind of output file formats: json and html. You can define the format you want with `-o html` or `-o json`.
 
-There are two important steps:
+If you want to download all seasons up to date, run `python download.py`. If you want to download a specific season in html files, say season 34, run `python download.py -s 34 -o html`.
 
-1. Downloading the game files from the J! Archive website
-2. Parsing and inserting them into the database
+Examplary json output file is included [here](https://github.com/n33t1/jeopardy-parser/blob/master/2002-09-09.json). For each clue, we have the following attributes:
 
-The first step, downloading, will depend on the machine: the download script will use twice the number of available cores to download game files in parallel and will take around an hour to complete. The second step, parsing, should take ~30 minutes (on a 1.7 GHz Core i5 w/ 4 GB RAM). In total, you're looking at around 2 hours (probably less).
+* Jtype:
+  * "single": single jeopardy. Prices for the corresponding clue should be either 200, 400, 600, 800 or 1000.
+  * "double": daily doubles. Prices various. 
+  * "placeholder": clue was missing from J! Archive website. All other fields are defined as null. 
+* price
+* prompt
+* solution
 
-The complete download of the game files is ~350MB, and the resulting database file is ~50MB (although these numbers are qucikly outdated as the number of games increases).
-
-Querying the database
----------------------
-
-The database is split into 5 tables:
-
-| Table name        | What it holds                                          |
-| ----------------- | ------------------------------------------------------ |
-| `airdates`        | Airdates for the shows, indexed by game number         |
-| `documents`       | Mappings from clue IDs to clue text and answers        |
-| `categories`      | The categories                                         |
-| `clues`           | Clue IDs with metadata (game number, round, and value) |
-| `classifications` | Mappings from clue IDs to category IDs                 |
-
-To get all the clues along with their metadata:
-
-```sql
-SELECT clues.id, game, round, value, clue, answer
-FROM clues
-JOIN documents ON clues.id = documents.id
--- WHERE <expression>
-;
-```
-
-To get the category that a clue is in, given a clue id:
-
-```sql
-SELECT clue_id, category
-FROM classifications
-JOIN categories ON category_id = categories.id
--- WHERE <expression>
-;
-```
-
-To get everything (although it is better to pick and choose what you're looking for):
-
-```sql
-SELECT clues.id, clues.game, airdate, round, value, category, clue, answer
-FROM clues
-JOIN airdates ON clues.game = airdates.game
-JOIN documents ON clues.id = documents.id
-JOIN classifications ON clues.id = classifications.clue_id
-JOIN categories ON classifications.category_id = categories.id
--- WHERE <expression>
-;
-```
-
-License
--------
-
-This software is released under the MIT License. See the [LICENSE.md](LICENSE.md) file for more information.
+Each game contains the following fields:
+* keys: rounds in this game. If a game has keys equal to [1, 2], then it means that game onlys has Jeopardy! Round and Double Jeopardy! Round. 
+* 1: stands for Jeopardy! Round
+* 2: stands for Double Jeopardy! Round. Might be missing for some games. 
+* 3: stands for Final Jeopardy! Round. Might be missing for some games. 
--- download.py
+++ download.py
@@ -1,93 +1,155 @@
 #!/usr/bin/env python -OO
 # -*- coding: utf-8 -*-
 
-import itertools
 import os
 import urllib2
+from bs4 import BeautifulSoup
 import time
-import futures as futures  # In Python 3 we can use "import concurrent.futures as futures"
+import jparser
+import parser
+import argparse
+import sys
+import concurrent.futures as futures
 
-current_working_directory = os.path.dirname(os.path.abspath(__file__))
-archive_folder = os.path.join(current_working_directory, "j-archive")
-SECONDS_BETWEEN_REQUESTS = 5
 ERROR_MSG = "ERROR: No game"
-NUM_THREADS = 2  # Be conservative
-try:
-    import multiprocessing
-    # Since it's a lot of IO let's double # of actual cores
-    NUM_THREADS = multiprocessing.cpu_count() * 2
-    print 'Using {} threads'.format(NUM_THREADS)
-except (ImportError, NotImplementedError):
-    pass
+SECONDS_BETWEEN_REQUESTS = 3
+NUM_THREADS = 2 
 
+base_url = "http://j-archive.com/"
+base_page = urllib2.urlopen(base_url).read()
+soup = BeautifulSoup(base_page, "html.parser")
 
-def main():
-    create_archive_dir()
-    print "Downloading game files"
-    download_pages()
+def main(season, output_type):
+      try:
+            import multiprocessing 
+            # Since it's a lot of IO let's double # of actual cores
+            NUM_THREADS = multiprocessing.cpu_count() * 2
+            print 'Using {} threads'.format(NUM_THREADS)
+      except (ImportError, NotImplementedError):
+            pass
 
+      if season is not None:
+            parse_specific_season(season, output_type)
+      else:
+            parse_season(output_type)
 
-def create_archive_dir():
-    if not os.path.isdir(archive_folder):
-        print "Making %s" % archive_folder
-        os.mkdir(archive_folder)
+def parse_specific_season(season, output_type):
+      url = "http://www.j-archive.com/showseason.php?season=%s" % season
 
+      archive_folder = 'season_%s' % str(season)
+      create_archive_dir(archive_folder)
 
-def download_pages():
-    page = 1
-    with futures.ThreadPoolExecutor(max_workers=NUM_THREADS) as executor:
-        # We submit NUM_THREADS tasks at a time since we don't know how many
-        # pages we will need to download in advance
-        while True:
-            l = []
-            for i in range(NUM_THREADS):
-                f = executor.submit(download_and_save_page, page)
-                l.append(f)
-                page += 1
-            # Block and stop if we're done downloading the page
-            if not all(f.result() for f in l):
-                break
+      season_html = download_page(url)
+      print "Now download season %s" % str(season)
+      parse_game(season_html, archive_folder, output_type)
 
+def parse_season(output_type):
+      for line in soup.select('a[href^="showseason.php"]'):
+            url = line.get('href')
+            url = base_url + url
+            season = url.partition("season=")[2]
 
-def download_and_save_page(page):
-    new_file_name = "%s.html" % page
-    destination_file_path = os.path.join(archive_folder, new_file_name)
-    if not os.path.exists(destination_file_path):
-        html = download_page(page)
-        if ERROR_MSG in html:
-            # Now we stop
-            print "Finished downloading. Now parse."
-            return False
-        elif html:
-            save_file(html, destination_file_path)
-            time.sleep(SECONDS_BETWEEN_REQUESTS)  # Remember to be kind to the server
-    else:
-        print "Already downloaded %s" % destination_file_path
-    return True
+            archive_folder = 'season_%s' % str(season)
+            create_archive_dir(archive_folder)
 
+            season_html = download_page(url)
+            print "Now download season %s" % str(season)
+            parse_game(season_html, archive_folder, output_type)
 
-def download_page(page):
-    url = 'http://j-archive.com/showgame.php?game_id=%s' % page
-    html = None
-    try:
-        response = urllib2.urlopen(url)
-        if response.code == 200:
-            print "Downloading %s" % url
-            html = response.read()
-        else:
-            print "Invalid URL: %s" % url
-    except urllib2.HTTPError:
-        print "failed to open %s" % url
-    return html
+def parse_game(season_html, archive_folder, output_type):
+      td_al_left = season_html.find_all('td',{'align':'left'})
+      i = 1
+      with futures.ThreadPoolExecutor(max_workers=NUM_THREADS) as executor:
+            # We submit NUM_THREADS tasks at a time since we don't know how many
+            # pages we will need to download in advance
+            while i < len(td_al_left):
+                  l = []
+                  for _ in range(NUM_THREADS):
+                        td = td_al_left[i]
+                        date = td.a.contents[0]
+                        game_date = date.split()[2]
 
+                        url = td.a['href']
+                        game_id = url.partition("game_id=")[2]
 
-def save_file(html, filename):
-    try:
-        with open(filename, 'w') as f:
-            f.write(html)
-    except IOError:
-        print "Couldn't write to file %s" % filename
+                        print "Now download game %s" % str(game_date)
 
+                        f = executor.submit(download_and_save_page, url, game_date, archive_folder, output_type)
+                        l.append(f)
+                        i += 1
+                        if i == len(td_al_left):
+                              break
+
+                  # Block and stop if we're done downloading the page
+                  if not all(f.result() for f in l):
+                        break
+
+def download_and_save_page(url, game_date, archive_folder, output_type):
+      new_file_name = "%s.html" % game_date if output_type == 'html' else "%s.json" % game_date
+      destination_file_path = os.path.join(archive_folder, new_file_name)
+      if not os.path.exists(destination_file_path):
+            html = download_page(url)
+            if ERROR_MSG in html:
+                  # Now we stop
+                  print "Finished downloading. Now parse."
+                  return False
+            elif html:
+                  html_string = str(html)
+
+                  if output_type == 'html':
+                        save_file(html_string, destination_file_path)
+                  elif output_type == 'json':
+                        html_lxml = BeautifulSoup(html_string, 'lxml')
+                        parse_to_json(html_lxml, destination_file_path)
+                  else:
+                        raise NameError('File type not valid!')
+
+                  time.sleep(SECONDS_BETWEEN_REQUESTS)  
+      else:
+            print "Already downloaded %s" % destination_file_path
+      return True
+
+def download_page(url):
+      html = None
+      try:
+            response = urllib2.urlopen(url)
+            if response.code == 200:
+                  print "Downloading ", url 
+                  # html = response.read()
+                  html = BeautifulSoup(response, "html.parser")
+            else:
+                  print "Invalid URL: ", url
+      except urllib2.HTTPError:
+                  print "failed to open ", url
+      return html
+
+def save_file(html_string, filename):
+      try:
+            with open(filename, 'w') as f:
+                  f.write(html_string)
+      except IOError:
+            print "Couldn't write to file %s" % filename
+
+def parse_to_json(html_string, destination_file_path):
+      test = jparser.Round(html_string, destination_file_path)
+      test.parseGame()
+            
+def create_archive_dir(season):
+      if not os.path.isdir(season):
+            os.mkdir(season)
 
 if __name__ == "__main__":
-    main()
+      parser = argparse.ArgumentParser(description='Script to download and parse jeopardy games.')
+      parser.add_argument('-o', '--output',
+                        help='output file type, currently support html or json',
+                        default='json',
+                        choices=set(('html', 'json')))
+
+      # still need to check range for season
+      parser.add_argument('-s', '--season',
+                        help='jeopardy season to download',
+                        type=int)
+
+      args = parser.parse_args()
+
+      main(args.season, args.output)
